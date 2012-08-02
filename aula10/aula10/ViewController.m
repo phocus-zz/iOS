c4ba0695a99f7d1bd6bb5f39e71fefb5dd89c1fb
phocus <administrador@phocus.com.br>
2012-08-02 18:40:12 -0300

    2012-08-02 18:40

--- aula10/aula10/ViewController.m
+++ aula10/aula10/ViewController.m
@@ -31,6 +31,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //Jean
     
     NSString *caminho = [[NSBundle mainBundle] pathForResource:@"musica" ofType:@"mp3"];
     audio = [[AVAudioPlayer alloc] initWithContentsOfURL:[NSURL fileURLWithPath:caminho] error:NULL];
